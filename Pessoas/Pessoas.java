517547e71372a06bac336753ecd443acc34047cc
Talsview <daviconcyline@gmail.com>
2025-10-07 20:59:55 -0300

    Pessoas.java

--- Pessoas/Pessoas.java
+++ Pessoas/Pessoas.java
@@ -7,6 +7,10 @@ public class Pessoas {
     
     public void mostrarInfo(){
     System.out.println("Nome: " + nome + " Idade: " + idade + " Altura: " + altura + " Nacionalidade :" + nacionalidade);
+
+        public int niver(){
+            return idade + 1;
     
+        }
     }
 }
